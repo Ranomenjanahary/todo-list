57121df5203685d61af8c1efc116c90bd2925c52
Ranomenjanahary <hei.jaonary@gmail.com>
2023-02-01 07:52:11 +0300

    feat: add todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
+// import React, { Component, useState } from 'react';
+
+// class IncrementC extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//   }
+
+//   handleIncrement() {
+//     this.setState({ count: this.state.count + 1 });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={() => this.handleIncrement()}>Increment</button>
+//         <p>Count: {this.state.count}</p>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+// function IncrementF() {
+//   const [ count, setCount] = useState(0);
+
+//   const handleIncrement = () => {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleIncrement}>Increment</button>
+//       <p>Count: {count}</p>
+//     </div>
+//   );
+// }
+
+
+// function App() {
+//   return (
+//     <>
+//       <IncrementC/>
+//       <IncrementF/>
+//       <h1>MAMPALAHELO ANAKAY</h1>
+//     </>
+
+//   );
+// }
+// export default App;
+
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function TodoList() {
+  const [todo, setTodo] = useState('');
+  const [doing, setDoing] = useState([]);
+  const [done, setDone] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDoing([...doing, todo]);
+    setTodo('');
+  }
+
+  const handleDelete = (index) => {
+    const newDoing = [...doing];
+    newDoing.splice(index, 1);
+    setDoing(newDoing);
+  }
+
+  const handleCheck = (index) => {
+    const currentTodo = doing[index];
+    handleDelete(index);
+    setDone([...done, currentTodo]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          value={todo} 
+          onChange={e => setTodo(e.target.value)} onKeyDown={(e)=>{e.key === 'Enter' && handleSubmit()}}
+          placeholder="Add task"
+        />
+      </form>
+      <div className='todo'>
+        <h3>TODO</h3>
+        <ul>
+          {doing.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <input 
+                type="checkbox" 
+                onClick={() => handleCheck(index)} 
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='done'>
+        <h3>DONE</h3>
+        <ul>
+          {done.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
+
